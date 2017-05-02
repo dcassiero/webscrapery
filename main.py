e4859ce8e2315640c935adbd19f2d102dafbf692
Daniel Cassiero <daniel.cassiero@gmail.com>
2017-05-02 14:48:58 -0400

    using health page for an example

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
 from bs4 import BeautifulSoup
 import requests
 
+params = []
+r = requests.get("https://stage-services.icg360.org/cxcat/health", params=params)
+
+soup = BeautifulSoup(r.text, "html.parser")
+results = soup.findAll("label", {"class": "control-label"})
+
+print(results)
